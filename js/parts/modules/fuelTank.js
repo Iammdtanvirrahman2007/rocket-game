ab581188a2eef373a55971cde488c940b4f6d355
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 08:13:01 +0600

    Update fuelTank.js

--- js/parts/modules/fuelTank.js
+++ js/parts/modules/fuelTank.js
@@ -1,15 +1,7 @@
-export const FuelTank = {
-    id: 'fuel-tank-200',
+export const fuelTank = {
+    id: 'fuel-tank',
     name: 'FL-T200 Fuel Tank',
     type: 'fuel',
-    stats: { mass: 2.0, fuel: 200, thrust: 0 },
-    
-    createSidebarItem() {
-        const el = document.createElement('div');
-        el.className = 'part-item tank-shape';
-        el.draggable = true;
-        el.dataset.id = this.id;
-        el.innerHTML = `<span>${this.name}</span>`;
-        return el;
-    }
+    mass: 1.5,
+    icon: '⛽'
 };
