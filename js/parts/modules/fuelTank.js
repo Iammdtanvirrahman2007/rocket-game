b4e93cab7e6a3ae9522ff7d10f129c71a0a26344
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 09:54:40 +0600

    Update fuelTank.js

--- js/parts/modules/fuelTank.js
+++ js/parts/modules/fuelTank.js
@@ -1,11 +1,29 @@
 export const fuelTank = {
-    id: 'fuel-tank',
-    name: 'FL-T200 Fuel Tank',
-    type: 'fuel',
 
-    image: 'assets/parts/fuel-tank.png',
+    id: "fuel-tank",
+
+    name: "Fuel Tank",
+
+    type: "tank",
+
+    image: "./assets/parts/fuel_tank.png",
 
     stats: {
         mass: 1.5
+    },
+
+    attachNodes: {
+
+        top: true,
+        bottom: true,
+        left: true,
+        right: true
+
+    },
+
+    size: {
+        width: 120,
+        height: 140
     }
+
 };
