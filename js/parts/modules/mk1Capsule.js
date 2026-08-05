5290679e84ae4c7c27f81b38d6fc63a1068a53d5
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 08:12:45 +0600

    Update mk1Capsule.js

--- js/parts/modules/mk1Capsule.js
+++ js/parts/modules/mk1Capsule.js
@@ -1,16 +1,7 @@
-export const Mk1Capsule = {
+export const mk1Capsule = {
     id: 'mk1-capsule',
-    name: 'Command Capsule',
-    type: 'command',
-    stats: { mass: 1.2, fuel: 0, thrust: 0 },
-    
-    // সাইডবারের জন্য আইটেম তৈরি করার ফাংশন
-    createSidebarItem() {
-        const el = document.createElement('div');
-        el.className = 'part-item capsule-shape';
-        el.draggable = true; // ড্র্যাগ করা যাবে
-        el.dataset.id = this.id;
-        el.innerHTML = `<span>${this.name}</span>`;
-        return el;
-    }
+    name: 'Mk1 Command Pod',
+    type: 'capsule',
+    mass: 0.8,
+    icon: '🧑‍🚀'
 };
