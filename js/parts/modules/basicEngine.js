e927c130289f640c2c1de1ef9a3119b462fa6df5
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 08:13:15 +0600

    Update basicEngine.js

--- js/parts/modules/basicEngine.js
+++ js/parts/modules/basicEngine.js
@@ -1,15 +1,7 @@
-export const BasicEngine = {
+export const basicEngine = {
     id: 'basic-engine',
-    name: 'Reliant Engine',
+    name: 'Reliant Liquid Engine',
     type: 'engine',
-    stats: { mass: 1.5, fuel: 0, thrust: 250 },
-    
-    createSidebarItem() {
-        const el = document.createElement('div');
-        el.className = 'part-item engine-shape';
-        el.draggable = true;
-        el.dataset.id = this.id;
-        el.innerHTML = `<span>${this.name}</span>`;
-        return el;
-    }
+    mass: 2.0,
+    icon: '🔥'
 };
