33e4c2460f8d4bbcac37c99a27b932a39074e1f9
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 09:54:23 +0600

    Update mk1Capsule.js

--- js/parts/modules/mk1Capsule.js
+++ js/parts/modules/mk1Capsule.js
@@ -1,11 +1,29 @@
 export const mk1Capsule = {
-    id: 'mk1-capsule',
-    name: 'Mk1 Command Pod',
-    type: 'capsule',
 
-    image: 'assets/parts/mk1-capsule.png',
+    id: "mk1-capsule",
+
+    name: "MK-1 Capsule",
+
+    type: "capsule",
+
+    image: "./assets/parts/mk1_capsule.png",
 
     stats: {
         mass: 0.8
+    },
+
+    attachNodes: {
+
+        top: false,
+        bottom: true,
+        left: false,
+        right: false
+
+    },
+
+    size: {
+        width: 120,
+        height: 120
     }
+
 };
