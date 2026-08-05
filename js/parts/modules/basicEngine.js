38a4f20ff81a6f88d3c31a88ab05be5fe7ebfc1c
Iammdtanvirrahman2007 <mdtanvirrahman2021a@gmail.com>
2026-08-05 09:54:58 +0600

    Update basicEngine.js

--- js/parts/modules/basicEngine.js
+++ js/parts/modules/basicEngine.js
@@ -1,11 +1,29 @@
 export const basicEngine = {
-    id: 'basic-engine',
-    name: 'Reliant Liquid Engine',
-    type: 'engine',
 
-    image: 'assets/parts/basic-engine.png',
+    id: "basic-engine",
+
+    name: "Basic Engine",
+
+    type: "engine",
+
+    image: "./assets/parts/basic_engine.png",
 
     stats: {
-        mass: 2.0
+        mass: 1.2
+    },
+
+    attachNodes: {
+
+        top: true,
+        bottom: false,
+        left: false,
+        right: false
+
+    },
+
+    size: {
+        width: 120,
+        height: 90
     }
+
 };
